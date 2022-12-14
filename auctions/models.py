204ac14012b6ca286f5068d63b4b4e06dc115245
CaioSmidi <caiosmidi2001@gmail.com>
2022-12-14 12:23:20 +0100

    first commit, create listing page exists and a start of the listings pages itselves, nothing done for the style yet

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,57 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# class Category(models.Model):
+#     category = models.CharField(max_length=64)
+#
+#     def __str__(self):
+#         return f"{self.category}"
+
+
+class Listing(models.Model):
+
+    categories = [("TENN", "Tennis"), ("FOOT", "Football"), ("BASE", "Baseball"), ("BASK", "Basketball"), ("HOCK", "Hockey"), ("OTHER", "Other Sports")]
+
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    image_URL = models.URLField(blank=True)
+    starting_bid = models.DecimalField(max_digits=64, decimal_places=2)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller")
+    active = models.BooleanField(default=True)
+    category = models.CharField(max_length=64, choices=categories, default="TENN")
+    watchlist = models.ManyToManyField(User, blank=True, related_name="watchlist_item")
+
+    def __str__(self):
+        return f"Listing {self.id}: {self.title}, sold by {self.owner}"
+
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    price_bid = models.DecimalField(max_digits=64, decimal_places=2)
+
+
+    def __str__(self):
+        return f"{self.bidder} bid on {self.listing} for €{self.price_bid}"
+
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(blank=False)
+
+
+    def __str__(self):
+        return f"Comment {self.id} on listing {self.listing} written by {self.text}"
+
+
+# class Watchlist(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     item = models.ForeignKey(Listing, on_delete=models.CASCADE)
+#
+#
+#
+#     def __str__(self):
+#         return f"{self.item} on Watchlist of {self.user}"
--- auctions/views.py
+++ auctions/views.py
@@ -3,13 +3,145 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Listing, Bid, Comment
+
+
+class NewListingForm(forms.ModelForm):
+
+    title = forms.CharField(max_length=64)
+    description = forms.CharField(max_length=128)
+    starting_bid = forms.DecimalField(max_digits=64, decimal_places=2)
+    image_URL = forms.URLField(required=False)
+    category = forms.ChoiceField(required=False, choices=Listing.categories)
+
+    class Meta:
+        model = Listing
+        fields = ["title", "description", "starting_bid", "image_URL", "category"]
+
+class BiddingForm(forms.ModelForm):
+
+    class Meta:
+        model = Bid
+        fields = ["price_bid"]
+
+
+class PlaceCommentForm(forms.ModelForm):
+    pass
+
+    class Meta:
+        model = Comment
+        fields = ["listing", "commenter", "comment"]
 
 
 def index(request):
-    return render(request, "auctions/index.html")
 
+    listings = Listing.objects.filter(active=True)
+
+    return render(request, "auctions/index.html", {
+        "active_listings": listings,
+    })
+
+
+def create_listing(request):
+    if request.method == "POST":
+        form = NewListingForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            starting_bid = form.cleaned_data["starting_bid"]
+            image_URL = form.cleaned_data["image_URL"]
+            category = form.cleaned_data["category"]
+            user = request.user
+
+            listing = Listing(title = title, description = description, image_URL = image_URL, starting_bid = starting_bid, category = category, owner = user)
+            listing.save()
+
+            return HttpResponseRedirect(reverse(index))
+        else:
+            return render(request, "auctions/create_listing.html", {
+                "form": form,
+                "categories": Listing.categories
+            })
+    else:
+        return render(request, "auctions/create_listing.html", {
+            "form": NewListingForm(),
+            "categories": Listing.categories
+        })
+
+
+def listing(request, listing_id):
+
+    listing = Listing.objects.get(pk=listing_id)
+    user = User.objects.get(pk=request.user.id)
+
+    if request.user.is_authenticated:
+
+        if listing.active:
+
+            highest_bid = Bid.objects.filter(listing=listing_id).order_by("price_bid").first()
+
+            if request.user.id == listing.owner.id:
+                return render(request, "auctions/control_listing.html", {
+                    "price": highest_bid
+                })
+
+            else:
+
+                return render(request, "auctions/listing.html", {
+                    "title": listing.title,
+                    "active": listing.active,
+                    "image": listing.image_URL,
+                    "description": listing.description,
+                    "price": highest_bid,
+                    "owner": listing.owner
+                })
+
+        else:
+
+            if request.user.id == listing.owner.id:
+                return render(request, "auctions/closed_listing_seller.html", {
+
+                })
+
+            else:
+                return render(request, "auctions/closed_listing_user.html", {
+
+                })
+
+    else:
+        pass
+
+
+    # listing = Listing.objects.get(id=listing_id)
+    #
+    # if listing.active:
+    #
+    #     comments = Comment.objects.filter(listing=listing_id)
+    #
+    #     return render(request, "auctions/listing.html", {
+    #         "listing": listing
+    #     })
+    #     if request.user.is_authenticated:
+    #         watchlist_listing = Watchlist.objects.get(listing=listing_id, user=User.objects.get(pk=request.user.id))
+    #         if watchlist_listing is None:
+    #             listing_on_watchlist = False
+    #         else:
+    #             listing_on_watchlist = True
+    #     else:
+    #         listing_on_watchlist = False
+    #
+    #
+    # else:
+    #     pass
+
+
+def watchlist(request):
+    pass
+
+def categories(request):
+    pass
 
 def login_view(request):
     if request.method == "POST":
